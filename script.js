f38f540b1618297f727fe6d9902f94d78590b678
DevonRem <DevonReminick@gmail.com>
2023-07-06 14:13:17 -0400

    Create calculator visuals onscreen with HTML and CSS, and basic javascript functions are added

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstnum;
+let operator;
+let secondnum; //check this later, might cause problems
+let displaynum = 0;
+
+function operate(num1, operator, num2) {
+    //operator will decide which function above to call
+    //maybe add eventlistener on each of the buttons like +, -, and then set operator to
+    //whatever when when they are clicked?
+    //also need to change the text in the results class when any button clicked
+
+}
