67319439019eabf191138efa321ffe090568702e
DevonRem <DevonReminick@gmail.com>
2023-07-07 14:49:32 -0400

    Add functioning numbers on screen and clear button. Fix some CSS bugs.

--- script.js
+++ script.js
@@ -20,9 +20,43 @@ let secondnum; //check this later, might cause problems
 let displaynum = 0;
 
 function operate(num1, operator, num2) {
-    //operator will decide which function above to call
-    //maybe add eventlistener on each of the buttons like +, -, and then set operator to
-    //whatever when when they are clicked?
-    //also need to change the text in the results class when any button clicked
+    if (operator === '+') {
+        add(num1, num2);
+    }
+    else if (operator === '-') {
+        subtract(num1, num2);
+    }
+    else if (operator === '*') {
+        multiply(num1, num2);
+    }
+    else if (operator === '/') {
+        divide(num1, num2);
+    }
+}
 
+const results = document.querySelector('.results');
+const number = document.querySelectorAll('.number');
+const Coperator = document.querySelector('.Coperator');
+const divoperator = document.querySelector('.divoperator');
+const multoperator = document.querySelector('.multoperator');
+const minoperator = document.querySelector('.minoperator');
+const addoperator = document.querySelector('.addoperator');
+const equaloperator = document.querySelector('.equaloperator');
+
+for(let i=0; i< number.length; i++) {
+    number[i].addEventListener('click', display);
+}
+
+Coperator.addEventListener('click', display);
+
+function display(e) {
+    if(results.textContent === '0' && number || results.textContent === '+') {
+        results.textContent = e.target.innerText;
+    }
+    else{
+        results.textContent += e.target.innerText;
+    }
+    if(e.target.innerText === 'C') {
+        results.textContent = '0';
+    }
 }
